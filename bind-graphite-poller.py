a13e73f3f4656952517cf0320e9ac95717247f66
Jeffrey Forman <jforman@malbec.home.jeffreyforman.net>
2013-01-13 09:33:10 -0500

    add first round of error handling and log levels

--- bind-graphite-poller.py
+++ bind-graphite-poller.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import logging
 import pickle
 import socket
 import struct
@@ -8,10 +9,19 @@ import time
 import urllib2
 import xml.etree.ElementTree as ET
 
+LOGGING_FORMAT = "%(asctime)s : %(levelname)s : %(message)s"
+
+class PollerError(Exception):
+    pass
 
 def get_bind_stats_xml(bindhostport):
     """Given a host:port, fetch XML from BIND statistics port."""
-    req = urllib2.urlopen("http://%s" % bindhostport)
+    try:
+        req = urllib2.urlopen("http://%s" % bindhostport)
+    except urllib2.URLError, u_error:
+        logging.error("Unable to query BIND (%s) for statistics. Reason: %s.", bindhostport, u_error)
+        raise PollerError
+
     xml_data = req.read()
     return xml_data
 
@@ -29,6 +39,7 @@ def send_server_stats(args, now, bind_host, bind_xml):
         value = int(request.find("counter").text)
         stats.append((metric, (now, value)))
 
+    logging.debug("Server Statistics for %s: %s", bind_host, stats)
     send_to_carbon(args.carbonhostport, stats)
 
 def send_zones_stats(args, now, bind_host, bind_xml):
@@ -45,6 +56,7 @@ def send_zones_stats(args, now, bind_host, bind_xml):
             metric = "dns.%s.zone.%s.%s" % (bind_host, zone_compiled, counter.tag)
             stats.append((metric, (now, value)))
 
+    logging.debug("Zone Statistics for %s: %s", bind_host, stats)
     send_to_carbon(args.carbonhostport, stats)
     
 def send_memory_stats(args, now, bind_host, bind_xml):
@@ -55,17 +67,25 @@ def send_memory_stats(args, now, bind_host, bind_xml):
         metric = "dns.%s.memory.%s" % (bind_host, element.tag)
         stats.append((metric, (now, value)))
 
+    logging.debug("Memory Statistics for %s: %s", bind_host, stats)
     send_to_carbon(args.carbonhostport, stats)
 
 def send_to_carbon(carbonhostport, stats):
+    logging.debug("Pickling statistics to %s", carbonhostport)
     (carbon_host, carbon_port) = carbonhostport.split(":")
     payload = pickle.dumps(stats)
     header = struct.pack("!L", len(payload))
     message = header + payload
-    carbon_sock = socket.create_connection((carbon_host, carbon_port), 10)
-    carbon_sock.sendall(message)
-    carbon_sock.close()
-
+    try:
+        logging.debug("Opening connection to Carbon at %s", carbonhostport)
+        carbon_sock = socket.create_connection((carbon_host, carbon_port), 10)
+        logging.debug("Sending statistics to Carbon.")
+        carbon_sock.sendall(message)
+        logging.debug("Done sending statistics to Carbon.")
+        carbon_sock.close()
+        logging.debug("Closing connection to Carbon.")
+    except socket.error, s_error:
+        logging.error("Error sending to Carbon %s. Reason : %s", carbonhostport, s_error)
 
 def main():
     parser = argparse.ArgumentParser(description="Parse BIND statistics and pass them off to Graphite.")
@@ -77,21 +97,40 @@ def main():
                         type=int,
                         default=60,
                         help="Time between polling BIND for statistics and sending to Graphite. Default: 60.")
+    parser.add_argument("-v", "--verbose",
+                        choices=[1, 2, 3],
+                        type=int,
+                        default=2,
+                        help="Verbosity of output. 1:ERROR, 2:INFO (Default), 3:DEBUG.")
 
     args = parser.parse_args()
+    if args.verbose == 1:
+        logging_level = logging.ERROR
+    elif args.verbose == 3:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+
+    logging.basicConfig(format=LOGGING_FORMAT, level=logging_level)
     (bind_host, bind_port) = args.bindhostport.split(":")
     stats = []
 
     while True:
         start_timestamp = time.time()
-        print "Sending BIND statistics to carbon %s. Interval: %d seconds." % (args.carbonhostport, args.interval)
-        bind_xml = ET.fromstring(get_bind_stats_xml(args.bindhostport))
-        send_server_stats(args, int(start_timestamp), bind_host, bind_xml)
-        send_zones_stats(args, int(start_timestamp), bind_host, bind_xml)
-        send_memory_stats(args, int(start_timestamp), bind_host, bind_xml)
-        elapsed_time = time.time() - start_timestamp
-        print "Finished sending BIND statistics to carbon. (Elaped time: %.2f seconds.)" % elapsed_time
-        time.sleep(args.interval)
+        logging.info("Gathering statistics to send to Carbon %s. Interval: %d seconds.", args.carbonhostport, args.interval)
+        try:
+            bind_xml = ET.fromstring(get_bind_stats_xml(args.bindhostport))
+        except PollerError:
+            logging.error("Error encountered, skipping this iteration.")
+            pass
+        else:
+            send_server_stats(args, int(start_timestamp), bind_host, bind_xml)
+            send_zones_stats(args, int(start_timestamp), bind_host, bind_xml)
+            send_memory_stats(args, int(start_timestamp), bind_host, bind_xml)
+            elapsed_time = time.time() - start_timestamp
+            logging.info("Finished sending BIND statistics to carbon. (Elaped time: %.2f seconds.)", elapsed_time)
+        finally:
+            time.sleep(args.interval)
     
 
 if __name__ == "__main__":
